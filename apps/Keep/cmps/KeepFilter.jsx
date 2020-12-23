ea540fbaefe756778e63d9a3e0be8748f42d6fa1
kupersd <kupersd@gmail.com>
2020-12-23 20:59:55 +0200

    keep filter and wrap

---
+++ apps/Keep/cmps/KeepFilter.jsx
@@ -0,0 +1,36 @@
+export class KeepFilter extends React.Component {
+
+    state = {
+        filterBy: {
+            freeText: '',
+            type: ''
+        }
+    };
+
+    handleChange = (ev) => {
+        const callback = () => {
+            this.props.setFilter(this.state.filterBy);
+        };
+        const filterBy = { ...this.state.filterBy }
+        filterBy[ev.target.name] = ev.target.value;
+        this.setState({ filterBy }, callback);
+    };
+
+    render() {
+        return <section className="keep-filter">
+            <input type="text" name="freeText"
+                value={this.state.filterBy.freeText}
+                placeholder="Search"
+                autoComplete="off"
+                onChange={this.handleChange} />
+            <select name="type" value={this.state.filterBy.type}
+                onChange={this.handleChange}>
+                <option value="">All</option>
+                <option value="noteText">Text</option>
+                <option value="noteImg">Image</option>
+                <option value="noteTodos">Todos</option>
+            </select>
+        </section>;
+    }
+
+}

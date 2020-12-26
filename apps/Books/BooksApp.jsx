88d0d98d61061e34a6b8d99b483f1b6f42ddce7b
Ori Weinstock <ori.weinstock@gmail.com>
2020-12-27 01:05:19 +0200

    git router

--- apps/Books/BooksApp.jsx
+++ apps/Books/BooksApp.jsx
@@ -3,21 +3,17 @@ import { BooksList } from './pages/BookApp.jsx'
 const Router = ReactRouterDOM.HashRouter;
 const { Route, Switch } = ReactRouterDOM;
 
-
-// Simple React Component
 export class BooksApp extends React.Component {
 
     render() {
         return (
-            <Router>
-                <section className="app">
-                    <Switch>
-                        <Route path="/books/book/:bookId" component={BookDetails} />
-                        <Route path="/books/about" component={About} />
-                        <Route path="/books" component={BooksList} />
-                    </Switch>
-                </section>
-            </Router>
+            <section className="app">
+                <Switch>
+                    <Route path="/books/book/:bookId" component={BookDetails} />
+                    <Route path="/books/about" component={About} />
+                    <Route path="/books" component={BooksList} />
+                </Switch>
+            </section>
         )
     }
 }
